45e5767f5d761632b7f093be8178c09857a9646f
awsdevopsonline <punnam.elite@gmail.com>
2026-02-02 09:02:01 +0530

    TEST

--- 03_variables.sh
+++ 03_variables.sh
@@ -4,3 +4,9 @@ echo  "Please Enter user name::"
 read USER_NAME 
 echo user name is $USER_NAME
 echo "Hello, $USER_NAME how are you doing today?"
+
+
+echo "Please enter passwrord::""
+read -s PASSWORD
+echo "Password is set to $PASSWORD"
+echo "Welcome, $USER_NAME! Your password has been set."
