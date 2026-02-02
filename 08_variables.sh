a3b3b8a624b0cd1511ce49860c1b054700a156c4
awsdevopsonline <punnam.elite@gmail.com>
2026-02-02 16:44:23 +0530

    TEST

--- 08_variables.sh
+++ 08_variables.sh
@@ -2,12 +2,13 @@
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
-
-echo "pls run with root user"
+    echo "pls run with root user"
+    exit 1
 fi
 
 
 echo "Installing NGINX web server"
 dnf install nginx -y
+
 
 
