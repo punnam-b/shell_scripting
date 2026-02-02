6fd29ed72f14010a109ba97ef79ac2bbb8e25c66
awsdevopsonline <punnam.elite@gmail.com>
2026-02-02 09:11:35 +0530

    TEST

--- 01_hello_world.sh
+++ 01_hello_world.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo "Hello, World!"
+
+PERSON="Punnam"
+echo "Hello ${PERSON}, welcome to Bash scripting!"
+echo "My name is $PERSON. I am learning Bash scripting."
--- 03_variables.sh
+++ 03_variables.sh
@@ -2,7 +2,7 @@
 
 echo  "Please Enter user name::"
 read USER_NAME 
-echo user name is ${USER_NAME}
+echo "user name is ${USER_NAME}"
 echo "Hello, ${USER_NAME} how are you doing today?"
 
 
