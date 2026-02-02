640b1801905dcd75299803ddb3faa3cfdddb70fd
awsdevopsonline <punnam.elite@gmail.com>
2026-02-02 16:21:15 +0530

    TEST

--- 07_variables.sh
+++ 07_variables.sh
@@ -6,9 +6,8 @@ NUMBER=25
 #-lt -> less than
 #-eq -> equal to
 #-ne -> not equal to
-if [ $NUMBER -lt 20 ]; then
-    echo " $NUMBER is greater than 20"
-fi
 if [ $NUMBER -gt 20 ]; then
+    echo " $NUMBER is greater than 20"
+elif 
     echo " $NUMBER is less than 20"
 fi
