71264ff89fd63c9bcfe556a6e3d7fe41ab9ebce6
awsdevopsonline <punnam.elite@gmail.com>
2026-02-02 08:58:02 +0530

    TEST

--- 02_variables.sh
+++ 02_variables.sh
@@ -6,3 +6,13 @@ echo "Hello, $PERSON1 how are you doing today?"
 echo "Hello, $PERSON2 how are you doing today?"
 echo "Hello, $PERSON1 and $PERSON2! Welcome to the bash scripting tutorial."
 echo "Goodbye, $PERSON1 and $PERSON2! See you next time."
+
+
+
+PERSON3=$1
+PERSON4=$2
+echo "Hello, $PERSON3 how are you doing today?"
+echo "Hello, $PERSON4 how are you doing today?"
+echo "Hello, $PERSON3 and $PERSON4! Welcome to the bash scripting tutorial."
+echo "Goodbye, $PERSON3 and $PERSON4! See you next time."
+
---
+++ 03_variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo  "Please Enter user name::"
+read USER_NAME 
+echo user name is $USER_NAME
+echo "Hello, $USER_NAME how are you doing today?"
